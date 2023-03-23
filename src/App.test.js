787b6a65add0fdca798b83d6053db3baea4f6eb0
Scott Hill <scott289@gmail.com>
2023-03-23 06:20:30 -0700

    fix the test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders create todo link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/create todo/i);
   expect(linkElement).toBeInTheDocument();
 });
